25105aede394ab23fa2dfddbea24a949d620247e
Maximiliano <maximilianokerps@gmail.com>
2024-09-16 01:14:36 -0300

    provideFirebaseApp.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,9 +22,6 @@ import { APP_ROUTES } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
-// * Environment.
-import { ENV } from '../env/environment';
-
 export const APP_CONFIGURATION: ApplicationConfig = {
 	providers: [
 		provideRouter(APP_ROUTES, withViewTransitions(), withComponentInputBinding()),
@@ -32,8 +29,18 @@ export const APP_CONFIGURATION: ApplicationConfig = {
 		provideHttpClient(withFetch()),
 		provideStore(ROOT_REDUCERS),
 		provideEffects(ROOT_EFFECTS),
-		provideFirebaseApp(() => initializeApp(ENV)),
+		provideFirebaseApp(() =>
+			initializeApp({
+				projectId: 'sisorg-interview',
+				appId: '1:648665171042:web:006d7b89deb8a1be0ebd3b',
+				storageBucket: 'sisorg-interview.appspot.com',
+				apiKey: 'AIzaSyDxXigCpsX9mqZvr1pT-E7KeLd97lppf0E',
+				authDomain: 'sisorg-interview.firebaseapp.com',
+				messagingSenderId: '648665171042'
+			})
+		),
 		provideFirestore(() => getFirestore()),
-		provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideClientHydration()
+		provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+		provideClientHydration()
 	]
 };

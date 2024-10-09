585957d3f82d2926bf2c6ea3f017eb0c982cf8e1
Maximiliano <maximilianokerps@gmail.com>
2024-10-09 03:58:25 -0300

    Remove animations and forms. Fix zone.js.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 
 // * Router.
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 // * Routes.
 import { APP_ROUTES } from './app.routes';
 
 export const APP_CONFIGURATION: ApplicationConfig = {
 	providers: [
-		provideRouter(APP_ROUTES, withViewTransitions(), withComponentInputBinding()),
+		provideRouter(APP_ROUTES),
 		provideHttpClient(withFetch()),
 		// provideExperimentalZonelessChangeDetection(),
 		provideZoneChangeDetection({ eventCoalescing: true }),

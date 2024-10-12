ada3aaea2f94c4dce4851fb0a956ae7dac3310db
Maximiliano <maximilianokerps@gmail.com>
2024-10-12 16:18:23 -0300

    Firebase hosting i18n enable. Routes disabled. Deploy success.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
-export class ROUTES {
-	public static readonly home = $localize`:@@home:home`;
-	// public  static  readonly  FrequentlyAskedQuestion = $localize `:@@frequently-asked-questions:preguntas-frecuentes` ;
-	// public  static  readonly  Contacto = $localize `:@@contact:contact` ;
-}
+// export class ROUTES {
+// 	public static readonly home = $localize`:@@home:home`;
+// 	// public  static  readonly  FrequentlyAskedQuestion = $localize `:@@frequently-asked-questions:preguntas-frecuentes` ;
+// 	// public  static  readonly  Contacto = $localize `:@@contact:contact` ;
+// }
 
 export const APP_ROUTES: Routes = [
 	// {
@@ -16,10 +16,10 @@ export const APP_ROUTES: Routes = [
 	//   path: 'homestead',
 	//   component: HomeComponent
 	// },
-  {
-    path: ROUTES.home,
-      loadComponent: async () => await import('./modules/home/home.component').then((c) => c.HomeComponent)
-  },
+  // {
+  //   path: ROUTES.home,
+  //     loadComponent: async () => await import('./modules/home/home.component').then((c) => c.HomeComponent)
+  // },
 	{
 		path: '**',
 		redirectTo: '',

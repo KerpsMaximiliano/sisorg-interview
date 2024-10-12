6673b76bb7497bd13a474e9e95eedb583ceb175a
Maximiliano <maximilianokerps@gmail.com>
2024-10-12 16:00:17 -0300

    Routes i18n.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,28 @@
 import { Routes } from '@angular/router';
 
+export class ROUTES {
+	public static readonly home = $localize`:@@home:home`;
+	// public  static  readonly  FrequentlyAskedQuestion = $localize `:@@frequently-asked-questions:preguntas-frecuentes` ;
+	// public  static  readonly  Contacto = $localize `:@@contact:contact` ;
+}
+
 export const APP_ROUTES: Routes = [
-  // {
-  //   // path: $localize `home`,
-  //   path: 'weqewqewqe',
-  //   loadComponent: async () => await import('./modules/home/home.component').then((c) => c.HomeComponent)
-  // },
-  // {
-  //   path: 'homestead',
-  //   component: HomeComponent
-  // },
+	// {
+	//   // path: $localize `home`,
+	//   path: 'weqewqewqe',
+	//   loadComponent: async () => await import('./modules/home/home.component').then((c) => c.HomeComponent)
+	// },
+	// {
+	//   path: 'homestead',
+	//   component: HomeComponent
+	// },
   {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
-  }
+    path: ROUTES.home,
+      loadComponent: async () => await import('./modules/home/home.component').then((c) => c.HomeComponent)
+  },
+	{
+		path: '**',
+		redirectTo: '',
+		pathMatch: 'full'
+	}
 ];
